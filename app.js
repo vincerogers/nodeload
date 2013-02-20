f08732028614054f876bab853e7bd46069d261b3
Vince Rogers <vince.j.rogers@gmail.com>
2013-02-19 22:02:18 -0800

    Update
    Signed-off-by: Vince Rogers <vince.j.rogers@gmail.com>

--- app.js
+++ app.js
@@ -1,71 +1,85 @@
 
-  /**
-   * Module dependencies.
-   */
+	/**
+	 * Module dependencies.
+	 */
 
-  var express = require('express')
-    , routes = require('./routes')
-    , user = require('./routes/user')
-    , http = require('http')
-    , path = require('path')
-    , $ = require('jquery');
+	var express = require('express')
+		, routes = require('./routes')
+		, user = require('./routes/user')
+		, http = require('http')
+		, path = require('path')
+		, $ = require('jquery');
 
-  var io = require('socket.io');
-  var fs = require('fs');
-  var app = express();
+	var io = require('socket.io');
+	var fs = require('fs');
+	var app = express();
 
-  app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('your secret here'));
-    app.use(express.session());
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
-  });
+	app.configure(function(){
+		app.set('port', process.env.PORT || 3000);
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'ejs');
+		app.use(express.favicon());
+		app.use(express.logger('dev'));
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(express.cookieParser('your secret here'));
+		app.use(express.session());
+		app.use(app.router);
+		app.use(express.static(path.join(__dirname, 'public')));
+	});
 
-  app.configure('development', function(){
-    app.use(express.errorHandler());
-  });
+	app.configure('development', function(){
+		app.use(express.errorHandler());
+	});
 
-  app.get('/', routes.index);
-  app.get('/users', user.list);
+	app.get('/', routes.index);
+	app.get('/users', user.list);
 
-  var server = http.createServer(app);
+	var server = http.createServer(app);
 
-  server.listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-  });
+	server.listen(app.get('port'), function(){
+		console.log("Express server listening on port " + app.get('port'));
+	});
 
-  var fileName;
-  var socketFileQueue = [];
-  var socketInProcess = [];
+	var fileName;
+	var socketFileQueue = [];
+	var socketInProcess = false;
 
-  var path = "c:\\inetpub\\wwwroot\\nodeload\\files\\";
-  io.listen(server).on('connection', function (socket) {
-    console.log(socket.id);
-    socket.on('startFile', function (data) {
-      fileName = data;
-      console.log(fileName);
-      fs.unlink(path + fileName);
-    });
+	var path = "c:\\inetpub\\wwwroot\\nodeload\\files\\";
 
-  socket.on('sendChunk', function (data) {
-    console.log(this.id);
-    fs.appendFile(path + fileName, data.data, function (err) {
-      if (err) 
-        throw err;
-      console.log(data.sequence + ' - The data was appended to file ' + fileName);
-    });
-  });
+	io.listen(server).on('connection', function (socket) {
+		socket.on('startFile', function (data) {
+			fileName = data;
+			console.log(fileName);
+			fs.unlink(path + fileName);
+		});
 
-    socket.on('endFile', function (data) {
-      console.log(data);
-      socket.disconnect();
-    });
+		socket.on('sendChunk', function (data, returnMsg) {	
+			console.log("Received sequence " + data.sequence );	
+			var decoded =  new Buffer(data.data, 'base64').toString('binary');
+			fs.appendFileSync(path + fileName, decoded, 'binary');
+			console.log(data.sequence + ' - The data was appended to file ' + fileName);
+			returnMsg("Received sequence " + data.sequence );
+		});
 
-  });
+		socket.on('endFile', function () {
+			//socket.disconnect();
+		});
+
+	});
+
+	function writeData()
+	{
+			if(socketInProcess || !socketFileQueue.length)
+				return;
+
+			socketInProcess = true;
+			var chunk = socketFileQueue.shift();
+			fs.appendFile(path + fileName, chunk.data, function (err) {
+				if (err) 
+					throw err;
+				socketInProcess = false;
+				console.log(chunk.sequence + ' - The data was appended to file ' + fileName);
+			});			
+			writeData();			
+	}
